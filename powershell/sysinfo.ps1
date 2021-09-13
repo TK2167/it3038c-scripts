20f32512d7681b3627ebf8265755ac39cc6ce8ea
TK2167 <klekchtn@mail.uc.edu>
2021-09-12 23:30:03 -0400

    powershell files

---
+++ powershell/sysinfo.ps1
@@ -0,0 +1,8 @@
+﻿function getIP {
+        (Get-NetIPAddress).IPv4Address | Select-String "192"
+        }
+$IP = getIP
+$User = $env:USERNAME
+$Body = "Test"
+
+Send-MailMessage -To "Klekchyant2167@gmail.com" -From "klekchyant216@gmail.com" -Subject "IT3038C Windows SysInfo" -Body $BODY -SmtpServer smtp.gmail.com -port 587 -UseSSL -Credential (Get-Credential)
